ab98beb468fdeaf0760b59c8da2d146d9a0a2092
Offor Ogechukwu <ogeoffor22@gmail.com>
2022-10-28 23:47:42 +0100

    Altschool Assignment 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
+  const [ calc, setCalc] = useState("");
+
+  
+
+	const createNumbers = () => {
+		const numbers = [];
+		for (let i = 1; i < 10; i++) {
+      numbers.push(
+        <button key={i}>{i}</button>        
+      )
+
+		}
+    return numbers;
+
+	}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <div className="display">
+          <span>(0)</span> 0
+        </div>
+        <div className="operators">
+          <button>/</button>
+          <button>*</button>
+          <button>+</button>
+          <button>-</button>
+
+		  <button>DEL</button>
+        </div>
+		<div className="numbers">
+      {createNumbers()}
+			<button>0</button>
+			<button>.</button>
+			<button>=</button>
+
+		</div>
+
+      </div>
     </div>
   );
 }
